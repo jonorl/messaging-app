e96abbaaaa8d6a592dc49755d52aa032099cd3cb
jonorl <39122230+jonorl@users.noreply.github.com>
2025-05-28 11:34:57 +0300

    Fix: cascading fix to allow deletion of users with favourites and messages

---
+++ server/prisma/migrations/20250528083235_adding_cascading/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "Favourite" DROP CONSTRAINT "Favourite_favouriteId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Favourite" DROP CONSTRAINT "Favourite_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_receiverId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_senderId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favourite" ADD CONSTRAINT "Favourite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favourite" ADD CONSTRAINT "Favourite_favouriteId_fkey" FOREIGN KEY ("favouriteId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
