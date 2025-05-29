de0bc8950b8cf1ab8f6904c44ff6ea8b35d66f05
jonorl <39122230+jonorl@users.noreply.github.com>
2025-05-29 12:44:04 +0300

    Fix: Removed sharp middleware as now attachments go to Cloudinary

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -9,7 +9,6 @@ const db = require("../db/queries");
 const multer = require("../controllers/multer");
 const { validateUser } = require("../controllers/formValidation");
 const { authenticateToken } = require("../controllers/authentication");
-const { validateImageResolution } = require("../controllers/sharp");
 
 // Import Utils
 const { onlineUsers } = require("../utils/onlineUsers");
@@ -127,7 +126,6 @@ mainRouter.post(
   "/api/v1/messages/",
   authenticateToken,
   multer.single("image"),
-  validateImageResolution,
   async (req, res) => {
     try {
       const senderId = req.body.senderId;
@@ -280,7 +278,6 @@ mainRouter.post("/api/v1/groups", authenticateToken, async (req, res) => {
 mainRouter.post("/api/v1/groups/:groupId/messages", 
   authenticateToken, 
   multer.single("image"), 
-  validateImageResolution,
   async (req, res) => {
     try {
       const { groupId } = req.params;
@@ -309,7 +306,6 @@ mainRouter.put(
   "/api/v1/users",
   authenticateToken,
   multer.single("avatar"),
-  validateImageResolution,
   async (req, res) => {
     try {
       const avatar = req.file ? `/assets/${req.file.filename}` : undefined;
